352a3126b8cd72e297f4ee244a7c1df0114666d5
Cecilia Milla <cmilla1020@gmail.com>
2022-11-07 12:37:33 -0500

    commit tree 5f9316cac0ac00006960a8ddfe7cc69d455b0206

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+function superbowlWin(record){
+  function isWin(record){
+     return record.result === "W"
+  }
+  const found = record.find(isWin)
+  if(found)
+    return found.year
+  else
+    return undefined
+}
